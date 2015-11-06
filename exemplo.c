3e343439d0589344c8c2bcf1122fa46fc5c2e747
CarsonGracie22 <pether.trabs@gmail.com>
2015-11-06 21:13:37 -0200

    Create exemplo.c

---
+++ exemplo.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main (void) {
+int i;
+float r;
+double dr;
+char c;
+i = 10;
+r = 1.0;
+dr = 3e3;
+c = 'a';
+printf ("i = %d\n", i);
+printf ("r = %f\n", r);
+printf ("df = %f\n", dr);
+printf ("c = %c\n", c);
+return 0;
+}
